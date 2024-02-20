012493e0a4ae0437a1c161596de55fe6fc72c20d
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-02-20 13:49:10 +0530

    Create Data Types.cpp

---
+++ Data Types.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a; //declaration
+    a=12; //initialisation
+    cout<< "Size of int : " << sizeof(a) << endl;
+    float b;
+    cout<< "Size of float : " << sizeof(b) << endl;
+    char c;
+    cout<< "Size of char : " << sizeof(c) << endl;
+    bool d;
+    cout<< "Size of bool : " << sizeof(d) << endl;
+    return 0;
+}
