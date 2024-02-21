6e3659a6866e55827b827f5badf744e55ed39af5
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-02-21 08:30:41 +0530

    Create Type Modifiers.cpp

---
+++ Type Modifiers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    short int si;
+    long int li;
+    cout<< "Size of short int : " << sizeof(si) << endl;
+    cout<< "Size of long int : " << sizeof(li) << endl;
+    return 0;
+}
