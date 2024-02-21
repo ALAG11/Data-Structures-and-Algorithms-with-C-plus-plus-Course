97c9b7f8b17904a03c3d5cd27d7705e995fa28d3
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-02-21 08:35:10 +0530

    New files added

--- Input.cpp
+++ Input.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout << "Hello World";
+    int amount1;
+    cin >> amount1;
+    int amount2;
+    cin >> amount2;
+    int sum = amount1 + amount2;
+    cout << sum;
     return 0;
 }
