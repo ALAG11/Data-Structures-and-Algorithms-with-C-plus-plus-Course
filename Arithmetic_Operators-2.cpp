1715695edea9625f893761beead98b93b545bc7f
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-14 09:41:13 +0530

    Create Arithmetic_Operators-2.cpp

---
+++ Arithmetic_Operators-2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    int j = 2;
+    int k;
+    k = i + j + i++ + j++ + ++i + ++j;
+    cout << i << " " << j << " " << " " << k << endl;
+return 0;
+}
