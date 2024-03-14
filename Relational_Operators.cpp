78f35b06cde043abeb895f9ab437c06f7b810126
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-14 09:41:50 +0530

    Create Relational_Operators.cpp

---
+++ Relational_Operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if (n > 10){
+        cout << "More than 10" << endl;
+    }
+    else if(n < 10){
+        cout << "Less than 10" << endl;
+    }
+    else{
+        cout << "Equal to 10" << endl;
+    }
+return 0;
+}
